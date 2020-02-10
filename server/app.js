8e0f0040932d19db1be1a1de23b31ebbbab73a84
franko4don <franko4don@yahoo.com>
2020-02-10 10:07:29 +0100

    added server to run react in production mode, as well as CI

---
+++ server/app.js
@@ -0,0 +1,19 @@
+//Require all that's needed to power this App
+//adding a few documentation
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require('cors');
+app.use(express.static("static"));
+app.use('/static', express.static('./../build;'));
+
+// allow cors
+app.use(cors());
+
+//default landing:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./../build", "index.html"));
+});
+//Running the server on Port 3000 default
+let PORT = 3002;
+app.listen(PORT, () => {console.log(`App is running on Port ${PORT}`)});
---
+++ server/parse-apps.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+      name        : "Abuchi",
+      script      : "npm start",
+      watch       : true,
+      merge_logs  : true,
+      cwd         : "/var/www/abuchi/server",
+     }]
+  }
