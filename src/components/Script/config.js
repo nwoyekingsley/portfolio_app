6d3026b0e46bbf9360e4e91cce313dadb79efc17
Ogbuifymark <ogbuifymark@gmail.com>
2020-03-05 11:48:55 +0100

    changed api url

--- src/components/Script/config.js
+++ src/components/Script/config.js
@@ -1,2 +1,2 @@
 // export const apiUrl = 'http://ogbuifymark-001-site2.btempurl.com/'
-export const apiUrl = 'http://ogbuifymark-001-site4.btempurl.com/'
+export const apiUrl = 'https://api.goterem.com/'
